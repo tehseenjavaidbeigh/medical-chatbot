84cc9d6c47e7f8b35bdf953572ba6b57b48b2352
tehseenjavaidbeigh <tehseenjavaidbeigh@gmail.com>
2026-02-28 18:10:53 +0530

    Folders and Files cr eated

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Creating the directory structure for the Medical Chatbot project
+mkdir -p src
+mkdir -p research
+#Creating the necessary files for the project
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory structure and files for the Medical Chatbot project have been created."
